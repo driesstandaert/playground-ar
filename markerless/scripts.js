c98bff4be6cebf8c07554c154065fbc102ec522e
driesstandaert <driesstandaert@gmail.com>
2020-08-18 19:14:06 +0100

    Change image

--- markerless/scripts.js
+++ markerless/scripts.js
@@ -33,32 +33,32 @@ AFRAME.registerComponent('play-all-model-animations', {
 });
 
 // Sky background + shader 
-AFRAME.registerComponent('sun-position-setter', {
-  init: function () {
-    var skyEl = this.el;
-    var orbitEl = this.el.sceneEl.querySelector('#orbit');
+// AFRAME.registerComponent('sun-position-setter', {
+//   init: function () {
+//     var skyEl = this.el;
+//     var orbitEl = this.el.sceneEl.querySelector('#orbit');
 
-    orbitEl.addEventListener('componentchanged', function changeSun (evt) {
-      var sunPosition;
-      var phi;
-      var theta;
+//     orbitEl.addEventListener('componentchanged', function changeSun (evt) {
+//       var sunPosition;
+//       var phi;
+//       var theta;
 
-      if (evt.detail.name !== 'rotation') { return; }
+//       if (evt.detail.name !== 'rotation') { return; }
 
-      sunPosition = orbitEl.getAttribute('rotation');
+//       sunPosition = orbitEl.getAttribute('rotation');
 
-      if(sunPosition === null) { return; }
+//       if(sunPosition === null) { return; }
 
-      theta = Math.PI * (- 0.5);
-      phi = 2 * Math.PI * (sunPosition.y / 360 - 0.5);
-      skyEl.setAttribute('material', 'sunPosition', {
-        x: Math.cos(phi),
-        y: Math.sin(phi) * Math.sin(theta),
-        z: -1
-      });
-    });
-  }
-});
+//       theta = Math.PI * (- 0.5);
+//       phi = 2 * Math.PI * (sunPosition.y / 360 - 0.5);
+//       skyEl.setAttribute('material', 'sunPosition', {
+//         x: Math.cos(phi),
+//         y: Math.sin(phi) * Math.sin(theta),
+//         z: -1
+//       });
+//     });
+//   }
+// });
 
 
 // Hide element in AR (for example background)
@@ -68,7 +68,8 @@ AFRAME.registerComponent('hide-in-ar-mode', {
       this.el.sceneEl.addEventListener('enter-vr', (ev) => {
           this.wasVisible = this.el.getAttribute('visible');
           if (this.el.sceneEl.is('ar-mode')) {
-              this.el.setAttribute('visible', false);
+              //this.el.setAttribute('visible', false);
+              this.el.style.opacity = .2;
           }
       });
       this.el.sceneEl.addEventListener('exit-vr', (ev) => {
