5ebf2f4c465eefe9095213df111e0ac1ff11369c
driesstandaert <driesstandaert@gmail.com>
2020-08-19 22:42:37 +0100

    Change image

--- markerless-river/scripts.js
+++ markerless-river/scripts.js
@@ -193,42 +193,34 @@ AFRAME.registerComponent('box-loader', {
 //   }
 // });
 
-const glsl = require('glslify');
-const vertexShader = glsl.file('../shaders/vertex.glsl');
-const fragmentShader = glsl.file('../shaders/fragment.glsl');
+// var vector = camera.getWorldDirection();
+// angle = THREE.Math.radToDeg( Math.atan2(vector.x,vector.z) );  
 
-AFRAME.registerComponent('material-displacement', {
-  /**
-   * Creates a new THREE.ShaderMaterial using the two shaders defined
-   * in vertex.glsl and fragment.glsl.
-   */
-  init: function () {
-    this.material  = new THREE.ShaderMaterial({
-      uniforms: {time: { value: 0.0 }},
-      vertexShader,
-      fragmentShader
-    });
-    this.el.addEventListener('model-loaded', () => this.update());
-  },
+// console.log(angle);
 
-  /**
-   * Apply the material to the current entity.
-   */
-  update: function () {
-    const mesh = this.el.getObject3D('mesh');
-    if (mesh) {
-      mesh.material = this.material;
-    }
-  },
 
-  /**
-   * On each frame, update the 'time' uniform in the shaders.
-   */
-  tick: function (t) {
-    this.material.uniforms.time.value = t / 1000;
+AFRAME.registerComponent('rotation-reader', {
+  tick: function () {
+    // `this.el` is the element.
+    // `object3D` is the three.js object.
+
+    // `rotation` is a three.js Euler using radians. `quaternion` also available.
+    // console.log(this.el.object3D.rotation);
+
+    // `position` is a three.js Vector3.
+    // console.log(this.el.object3D.position);
+
+    // var worldQuaternion = new THREE.Quaternion();
+    // var testje = this.el.object3D.getWorldQuaternion(worldQuaternion);
+    // console.log(testje);
+
+    var cameraEl = this.el
+var worldPos = new THREE.Vector3();
+worldPos.setFromMatrixPosition(cameraEl.object3D.matrixWorld);
+console.log(worldPos.x);
+    
   }
-  
-})
+});
 
 
 
