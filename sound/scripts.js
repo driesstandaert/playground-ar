7d74b0e69e38e291adaf36df3f427cfe7db3f6df
driesstandaert <driesstandaert@gmail.com>
2020-08-22 14:27:37 +0100

    Add sound ui

--- sound/scripts.js
+++ sound/scripts.js
@@ -248,7 +248,7 @@ window.onload = function () {
 
   btnClose.addEventListener('click', function () {
     overlay.classList.toggle('is-visible');
-    soundRiver.sound.fade(.1, 1, 1000);
+    //soundRiver.sound.fade(.1, 1, 1000);
   });
   
 
@@ -259,6 +259,19 @@ window.onload = function () {
     soundVoiceover.play();
     loadinganime = false; // intro animation until scene starts
   });
+
+       // Grab elements, create settings, etc.
+var video = document.getElementById('video');
+
+// Get access to the camera!
+if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    // Not adding `{ audio: true }` since we only want video now
+    navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+        //video.src = window.URL.createObjectURL(stream);
+        video.srcObject = stream;
+        video.play();
+    });
+}
 
 
 
