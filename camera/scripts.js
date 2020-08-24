bd12559959f04a742b5ae2e5bd812710c2e501f8
driesstandaert <driesstandaert@gmail.com>
2020-08-24 09:20:17 +0100

    Add sound ui

--- camera/scripts.js
+++ camera/scripts.js
@@ -260,6 +260,24 @@ window.onload = function () {
     loadinganime = false; // intro animation until scene starts
   });
 
+       // Grab elements, create settings, etc.
+var video = document.getElementById('video');
+
+// Get access to the camera!
+if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    // Not adding `{ audio: true }` since we only want video now
+    navigator.mediaDevices.getUserMedia({ 
+      video: { 
+        facingMode: 'environment' 
+      }
+    }).then(function(stream) {
+        //video.src = window.URL.createObjectURL(stream);
+        video.srcObject = stream;
+        video.play();
+    });
+}
+
+
 
     /////////////////
     // Logo animation
